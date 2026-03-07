a249c6dcf25ee5243e0d5f4fa9c04cc64b4760ba
minhajul islam <mm01882390860@gmail.com>
2026-03-07 14:14:29 +0600

    data load frome fake api

--- BPL-2026/src/App.jsx
+++ BPL-2026/src/App.jsx
@@ -4,15 +4,30 @@ import './index.css'
 import Navbar from './components/Navbar/Navbar'
 import Available from './components/Available/Available'
 import Selected from './components/Selected/Selected'
+import { Suspense } from 'react'
+
+const fetchPlayers = async () => {
+  const rest = await  fetch('/data.json')
+  const data = await rest.json()
+  return data
+}
+
 
 function App() {
+  const playerPromise = fetchPlayers()
 
   return (
     <>
       <div  className="bg-white min-h-screen">
         <Navbar />
-        <Available />
-        <Selected />
+        <Suspense fallback={<span className="loading loading-spinner text-error"></span>
+}>
+          <Available playerPromise={playerPromise} />
+        </Suspense>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Selected />
+        </Suspense>
+       
 
       </div>
      
--- BPL-2026/src/components/Available/Available.jsx
+++ BPL-2026/src/components/Available/Available.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { use } from 'react'
 
 
-function Available() {
+function Available({playerPromise}) {
+
+    const playerData = use(playerPromise);
+    console.log(playerData);
   return (
     <div>
         <h1 className='text-3xl font-bold  mt-10 text-green-400'>Available Players</h1>
